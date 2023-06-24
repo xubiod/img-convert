ad5be68eb9ede6249b0f7c2c812028a076a8ca14
Xubiod <xubiod.mail@gmail.com>
2023-06-24 14:26:25 -0400

    simplify jfif decoder
    length of byte array => const int

--- dedicated-decoder/jfif/jfif.go
+++ dedicated-decoder/jfif/jfif.go
@@ -7,18 +7,7 @@ import (
 	"io"
 )
 
-// From https://github.com/leotaku/mobi/blob/master/jfif/jfif.go
-var naiveJFIFHeader = []byte{
-	0xFF, 0xD8, // SOI
-	0xFF, 0xE0, // APP0 Marker
-	0x00, 0x10, // Length
-	0x4A, 0x46, 0x49, 0x46, 0x00, // JFIF\0
-	0x01, 0x02, // 1.02
-	0x00,       // Density type
-	0x00, 0x01, // X Density
-	0x00, 0x01, // Y Density
-	0x00, 0x00, // No Thumbnail
-}
+const jfifHeaderSize = 20
 
 func Decode(r io.Reader) (img image.Image, err error) {
 	rawData, err := io.ReadAll(r)
@@ -28,7 +17,7 @@ func Decode(r io.Reader) (img image.Image, err error) {
 
 	// Hijack the JFIF data to mash in a "Start of Image" marker
 	// This technically makes it a valid JPEG
-	var overwriteHere = int64(len(naiveJFIFHeader)) - 2
+	var overwriteHere = int64(jfifHeaderSize) - 2
 
 	// Cut off all the JFIF header except for two bytes that
 	// make the "Start of Image" marker
